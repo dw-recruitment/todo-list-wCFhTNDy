ac73ea64c654a551f196df6a5fd30577ba270625
Anonymous Candidate <anon@example.com>
2016-03-26 16:07:35 -0400

    Complete step 3: about page

--- src/democracy_works_exercise/handler.clj
+++ src/democracy_works_exercise/handler.clj
@@ -5,23 +5,33 @@
     [ring.adapter.jetty :as jetty]
     [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn template
+  "The most rudimentary of HTML template functions."
+  [body]
+  (str "<!DOCTYPE html>
+        <html lang=en>
+        <head>
+          <meta charset=utf-8>
+          <title>Under Construction!</title>
+        </head>
+        <body>"
+          body
+       "</body>
+        </html>"))
+
 (def under-construction-html
-  "<!DOCTYPE html>
-   <html lang=en>
-   <head>
-     <meta charset=utf-8>
-     <title>Under Construction!</title>
-   </head>
-   <body>
-     <!-- this kinda makes the valid HTML pointless,
+  "<!-- this kinda makes the valid HTML pointless,
           but is fun and works in all the browsers I tried... -->
      <h2><marquee>Under Construction!</marquee></h2>
-     <img alt=\"under construction yo!\" src=\"images/under_construction.gif\" /><br />
-   </body>
-   </html>")
+     <img alt=\"under construction yo!\" src=\"images/under_construction.gif\" /><br />")
+
+(def about
+  "<h2>About</h2>
+   <p>This is an exercise for the democracy.works interview process, and it is designed to gauge how well I know or can pick up Clojure and some common libraries to build a simple web application. Credit where credit is due: this text has been taken almost verbatim from the exercise description itself.</p>")
 
 (defroutes app-routes
-  (GET "/" [] under-construction-html)
+  (GET "/" [] (template under-construction-html))
+  (GET "/about" [] (template about))
   (route/not-found "Not Found"))
 
 (def app
